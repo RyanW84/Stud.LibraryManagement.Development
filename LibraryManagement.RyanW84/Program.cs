d80fa9ee9e316cfa19d15a9f44408c72748a9bec
Ryan W <ryanweavers@gmail.com>
2025-08-30 21:39:38 +0100

    Feature(Menu): Creating the Menu

---
+++ LibraryManagement.RyanW84/Program.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+while (true)
+{
+    var choice = AnsiConsole.Prompt(
+        new SelectionPrompt<MenuOption>()
+            .Title("What would you like to do next?")
+            .AddChoices(Enum.GetValues<MenuOption>())
+    );
+
+    switch (choice)
+    {
+        case MenuOption.ViewBooks:
+            AnsiConsole.MarkupLine("[bold red]You can view the books below.[/]");
+            break;
+        case MenuOption.AddBook:
+            AnsiConsole.MarkupLine("[bold red]You can add the book.[/]");
+            break;
+        case MenuOption.DeleteBook:
+            AnsiConsole.MarkupLine("[bold red]You can delete the book.[/]");
+            break;
+    }
+}
+
+enum MenuOption
+{
+    ViewBooks,
+    AddBook,
+    DeleteBook,
+}
