2a750b7eab1dfb11ec9dbdae1327b3d44115dc06
Ryan W <ryanweavers@gmail.com>
2025-08-30 21:47:28 +0100

    feat(menu): member field added for books

--- LibraryManagement.RyanW84/Program.cs
+++ LibraryManagement.RyanW84/Program.cs
@@ -1,7 +1,17 @@
 ﻿using Spectre.Console;
 
+// field
+List<string> books =
+[
+    "book1", "book2", "book3", "book4", "book5", "book6",
+    "book7", "book8", "book9", "book10", "book11", "book12", "book13", "book14", "book15",
+    "book16", "book17", "book18", "book19", "book20"
+];
+
 while (true)
 {
+    Console.Clear();
+
     var choice = AnsiConsole.Prompt(
         new SelectionPrompt<MenuOption>()
             .Title("What would you like to do next?")
@@ -11,7 +21,10 @@ while (true)
     switch (choice)
     {
         case MenuOption.ViewBooks:
-            AnsiConsole.MarkupLine("[bold red]You can view the books below.[/]");
+            AnsiConsole.MarkupLine("[bold yellow]List of Books:[/]");
+            foreach (var book in books) AnsiConsole.MarkupLine($"[cyan]{book}[/]");
+            AnsiConsole.MarkupLine("Press any key to continue...");
+            Console.ReadKey();
             break;
         case MenuOption.AddBook:
             AnsiConsole.MarkupLine("[bold red]You can add the book.[/]");
@@ -22,9 +35,9 @@ while (true)
     }
 }
 
-enum MenuOption
+internal enum MenuOption
 {
     ViewBooks,
     AddBook,
-    DeleteBook,
+    DeleteBook
 }
